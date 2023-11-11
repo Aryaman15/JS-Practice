4e171f208b0287f46799ca39f4f9cf82050c7055
Aryaman Verma <aryamanskate01@gmail.com>
2023-11-12 00:25:23 +0530

    1st practice file commited

---
+++ 01_variables.js
@@ -0,0 +1,16 @@
+const accountId=144553;
+let accountEmail="aryamanskate01@gmail.com"
+var accountPassword="12345"
+accountCity="Jaiput"
+
+// accountId=2; Not allowed
+
+console.log(accountId);
+
+accountEmail="hc@hc.com"
+accountPassword="2121123"
+accountCity="Bengaluru"
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
